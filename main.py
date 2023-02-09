6bbae03b46b84acc76fec5cb5dc187adf73534f9
Kamil Dej <kamil.dej@gmail.com>
2023-02-09 13:25:44 +0100

    add snake increase, eat apple, display score

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.locals import *
 import time
@@ -16,6 +18,11 @@ class Apple:
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = random.randint(0, 24) * SIZE
+        self.y = random.randint(0, 19) * SIZE
+
+
 class Snake:
     def __init__(self, parent_screen, lenght):
         self.lenght = lenght
@@ -24,6 +31,11 @@ class Snake:
         self.x = [SIZE]*lenght
         self.y = [SIZE]*lenght
         self.direction = 'down'
+
+    def increase_lenght(self):
+        self.lenght += 1
+        self.x.append(-1)
+        self.y.append(-1)
 
     def move_left(self):
         self.direction = 'left'
@@ -71,9 +83,27 @@ class Game:
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+
+        return False
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_lenght()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f'Score: {self.snake.lenght-2}', True, (255, 255 , 225))
+        self.surface.blit(score,(800,10))
 
     def run(self):
         running = True
@@ -99,10 +129,10 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-
             self.play()
 
             time.sleep(0.3)
+
 
 if __name__ == "__main__":
     game = Game()
